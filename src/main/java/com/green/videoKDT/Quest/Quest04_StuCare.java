2a0af29092832b3ab27f35c78b0692deb18b3b10
user <wltngus200@naver.com>
2024-04-01 01:09:31 +0900

    Hello!240331

--- src/main/java/com/green/videoKDT/Quest/Quest04_StuCare.java
+++ src/main/java/com/green/videoKDT/Quest/Quest04_StuCare.java
@@ -2,16 +2,30 @@ package com.green.videoKDT.Quest;
 
 public class Quest04_StuCare {
     public static void main(String[] args){
+        Member person=new Member(12345,"abcde","서울특별시 강남구 역삼동","student@school.com");
+        person.result();
 
     }
 }
 class Member{
-    /*id
-    pw
-    addres
-    email
-    */
-    Member(int id,String pw){
+    int id;
+    String pw;
+    String address;
+    String email;
 
+    Member(int id,String pw,String address,String email){
+        this.id=id;
+        this.pw=pw;
+        this.address=address;
+        this.email=email;
     }
+    void result(){
+        System.out.println("학생 정보");
+        System.out.println("----------------");
+        System.out.printf("ID: %s\n",id);
+        System.out.printf("PW: %s\n",pw);
+        System.out.printf("Address: %s\n",address);
+        System.out.printf("Email: %s\n",email);
+    }
+
 }

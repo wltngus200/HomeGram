2c1b258e35d0345c943ed4e0e41eb0d7ebbbe016
user <wltngus200@naver.com>
2024-04-01 01:15:13 +0900

    Hello!240331

--- src/main/java/com/green/videoKDT/Quest/Quest05_notyet.java
+++ src/main/java/com/green/videoKDT/Quest/Quest05_notyet.java
@@ -2,6 +2,35 @@ package com.green.videoKDT.Quest;
 
 public class Quest05_notyet {
     public static void main(String[] args){
-
+        /*Whale w = new Whale();
+        w.setName("고래");
+        w.setAge(5);
+        w.setSwim(true);
+        System.out.println("이름 : "+w.getName());
+        System.out.println("나이 : "+w.getAge());
+        System.out.println("수영여부 : "+w.getAge());*/
     }
+}
+class Animal{
+    //public class Animal{//퍼블릭 해야됨?
+        private int age;
+        private String name,sex;
+        public void setAge(int n){
+            age = n;
+        }
+        public void setName(String n){
+            name = n;
+        }
+        public void setSex(String n){
+            sex = n;
+        }
+        public int getAge(){
+            return age;
+        }
+        public String getName(){
+            return name;
+        }
+        public String getSex(){
+            return sex;
+        }
 }

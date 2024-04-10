7390245d315ed07e82f6c6f45d87c8ecd5b70385
user <wltngus200@naver.com>
2024-04-10 23:06:20 +0900

    Hello!240410

--- src/main/java/com/green/practiceAlone/Dalma/Card.java
+++ src/main/java/com/green/practiceAlone/Dalma/Card.java
@@ -7,11 +7,19 @@ public class Card {
     private String number;
     private boolean color;//t=black f=red
 
-    Card(String shape, String number, boolean color){
-        this.shape=shape;
-        this.number=number;
-        this.color=color;
+    //카드가 가지는 속성들 모양 색 번호 각각의 객체마다 고유한 성질을 가진다.
+    Card(String shape, String number, boolean color) {
+        this.shape = shape;
+        this.number = number;
+        this.color = color;
+        //초기화를 하면서 속성들에 어떠한 값을 넣을 지 정할 수 있다
+        //>>카드덱 클래스(파일)
     }
+
+    @Override
+    public String toString() {
+        return String.format("(%s) %s %s", (color ? "black" : "red"), shape, number);
+    }//카드라는 객체를 출력할 때 이렇게 보여달라.
 }
 class CardTest{
     public static void main(String[] args){
--- src/main/java/com/green/practiceAlone/Dalma/CardDeck.java
+++ src/main/java/com/green/practiceAlone/Dalma/CardDeck.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
-    protected List<Card> cards;
     String[] shape={"Heart","Diamond","Spade","Clover"};
-    boolean color=false;
+    boolean color=false;// 카드객체와 중복. 어느 값이 들어갔는지 모르겠지만 둘 중 어느 쪽을 지워도 에러.
+    private List<Card> cards;//카드라는 객체를 담는 리스트 생성 카드를 여러개 담는 것이 카드덱의 속성
+
     public CardDeck(){
-        cards=new ArrayList();
+        cards=new ArrayList<>();//선언해줘야 한다
         for(int i=0;i<shape.length;i++){
             for(int j=1;j<=13;j++){
                 String numberStr=switch(j){
@@ -18,15 +19,31 @@ public class CardDeck {
                     case 13->"K";
                     default->String.valueOf(j);
                 };if(shape[i]=="Spade"||shape[i]=="Clover"){color=true;};
-                Card card=new Card(shape[i],numberStr,color);
-                cards.add(card);
-            }//카드 출력에서 막힘
+                Card card=new Card(shape[i],numberStr,color);//for문 스코프 안에서만 살아있는 카드 객체
+                cards.add(card);//그 값을 카드덱 cards에 저장 //cards는 카드덱 바깥에 있어서 살아남은
+            }
+        }
+    }
+    void showCards(){
+        for(Card c:cards)
+        System.out.println(c);
+    }
+    void suffle(){
+        for(int i=0;i<cards.size();i++){
+        int random=(int)(Math.random()*cards.size());
+        Card tmp=cards.get(random);
+        cards.remove(cards.get(random));
+        cards.add(tmp);//발상은 좋았으나 골고루 섞이지 않음
+        //cards.get(i)=tmp 는 빨간줄 왜???
         }
     }
 }
 class CardDeckTest{
     public static void main(String[] args){
     CardDeck cd=new CardDeck();
-    System.out.println(cd);
+    //cd.showCards();
+    cd.suffle();
+    System.out.println("-----------------------");
+    cd.showCards();
     }
 }

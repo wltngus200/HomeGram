c6eea2a3231ae2dd769fcb5f458c82b76d6bc445
user <wltngus200@naver.com>
2024-05-18 13:36:20 +0900

    20240518

---
+++ HomeGram/src/main/java/com/green/java/homegram/HomeGramApplication.java
@@ -0,0 +1,13 @@
+package com.green.java.homegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeGramApplication.class, args);
+	}
+
+}
